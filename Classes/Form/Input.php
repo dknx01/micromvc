23bc02d0ddde95c9b9ba5f1d1d70024a70900adc
dknx01 <e.witthauer@gmail.com>
2013-04-07 10:17:50 +0200

    Basic files only

--- Classes/Form/Input.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Form_Input extends Form_Abstract
-{
-    public function main()
-    {
-        $this->addFormDefinition('systemName', array('optional' => false))
-             ->addFormDefinition('systemClassification', array('optional' => false));
-    }
-}
