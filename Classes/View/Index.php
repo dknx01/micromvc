d0aa8e160f724c1de9f73afcfb9cd8d9f81c1bd4
dknx01 <e.witthauer@gmail.com>
2013-04-07 10:17:50 +0200

    Basic files only

--- Classes/View/Index.php
+++ Classes/View/Index.php
@@ -1,14 +1,8 @@
 <div id="container">
 
-    <div id="center-container">
-        <div id="infovis"></div>
-    </div>
+    <div id="center-container"></div>
 
-    <div id="right-container">
-
-        <div id="inner-details"></div>
-
-    </div>
+    <div id="right-container"></div>
 
     <div id="log"></div>
 </div>
